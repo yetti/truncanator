ea7dc731643ec63b2fba51ea8b034a86aa1e8a65
Yetrina Battad <hello@yetti.io>
2022-05-28 11:47:55 +1000

    refactor: updates README.md and styles

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ It can also remove duplicates if required.
 
 ## Building
 
-truncanator uses [TailwindCSS](https://tailwindcss.com/) (version v3.0.24) and [TailwindUI](https://tailwindui.com/) .
+truncanator uses [TailwindCSS](https://tailwindcss.com/) (version v3.0.24) and [TailwindUI](https://tailwindui.com/).
 
-To use Tailwind without NodeJs, you will need to install the latest version of the [Tailwind CLI](https://github.com/tailwindlabs/tailwindcss/releases/latest) for your OS.
+To use Tailwind without NodeJs, you will need to install the latest version of the standalone [Tailwind CLI](https://github.com/tailwindlabs/tailwindcss/releases/latest) for your OS.
 
 ```bash
 # Install for arm64 macOS
@@ -15,13 +15,39 @@ curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/latest/download/t
 chmod +x tailwindcss-macos-arm64
 mv tailwindcss-macos-arm64 tailwind
 
-# Start a watcher
-./tailwind -i style.css -o assets/css/style.css --watch
+# Start a watcher for development
+./tailwind -i assets/css/style.css -o public/css/style.css --watch
 
-# Compile and minify your CSS for production
-./tailwind -i style.css -o assets/css/style.css --minify
+# Compile and minify the final CSS for production
+./tailwind -i assets/css/style.css -o public/css/style.css --minify
+```
+
+### Auto browser reload (optional)
+
+If you want to auto-reload your browser on file changes, there are various NodeJS packages that can help.
+
+#### Browsersync
+
+Install [Browsersync](https://browsersync.io/) globally:
+
+```bash
+npm install -g browser-sync
+
+# Ignore the output file compiled by Tailwind CLI
+browser-sync start --server --ignore "public/css/style.css"
+```
+
+#### live-server
+
+Install [live-server](http://tapiov.net/live-server/) globally:
+
+```bash
+npm install -g live-server
+
+# Start the server and ignore the output file compiled by Tailwind CLI
+live-server --ignore="public/css/style.css"
 ```
 
 ## Attribution
 
-* <a href="https://www.flaticon.com/free-icons/axe" title="axe icons">Axe icons created by kerismaker - Flaticon</a>
+* Axe icon by [Cultures icons created by Freepik - Flaticon](https://www.flaticon.com/free-icons/cultures)
---
+++ public/js/main.js
@@ -0,0 +1,99 @@
+document.addEventListener('alpine:init', () => {
+  window.Alpine = Alpine;
+})
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+  
+window.truncanator = function() {
+  return {
+    removePrefix: false,
+    removeSuffix: false,
+    removeDuplicates: false,
+
+    input: "",
+    output: "",
+
+    prefixes: "",
+    suffixes: "",
+
+    processing: false,
+
+    truncate() {
+      console.log('truncating...');
+      this.processing = true;
+
+      try {
+        let outputLines = [];
+
+        const inputLines = this.input.split(/\r?\n/);
+        inputLines.forEach(line => {
+          const unwrappedText = this.unwrapQuotedText(line);
+          let filename = this.getFilename(unwrappedText);
+
+          if (this.removePrefix) {
+            this.prefixes.split(',').forEach(prefix => {
+              filename = this.truncatePrefix(filename, prefix);
+            });
+          }
+
+          if (this.removeSuffix) {
+            this.suffixes.split(',').forEach(suffix => {
+              filename = this.truncateSuffix(filename, suffix);
+            })
+          }
+
+          outputLines = [
+            ...outputLines,
+            filename
+          ]
+        })
+
+        this.output = this.removeDuplicates ? [...new Set(outputLines)].join('\n') : outputLines.join('\n');
+      } catch (ex) {
+        console.error(ex);
+      }
+
+      sleep(2000);
+      
+      this.processing = false;
+      console.log('done!');
+    },
+
+    unwrapQuotedText(txt) {
+      return txt.replace(/(^"|"$)/g, '');
+    },
+
+    getFilename(path) {
+      return path.split('\\').pop().split('/').pop()
+    },
+
+    truncatePrefix(txt, prefix) {
+      return txt.replace(new RegExp('^' + prefix.trim()), '');
+    },
+
+    truncateSuffix(txt, suffix) {
+      return txt.replace(new RegExp(suffix.trim() + ".[^/.]+$"), '')
+    },
+
+    processingButton() {
+      if (this.processing) {
+        return `
+          <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          </svg>
+          Processing
+        `;
+      } else {
+        return `Process`;
+      }
+    },
+  }
+}
